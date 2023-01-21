9d3412d689a30a1ad729c1a37b028ed9d099e26a
Stephvenn <stephenshin792@hotmail.ca>
2023-01-21 00:53:04 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,21 +1,26 @@
 from tkinter import *
 
+# text is changed upon button click
 def onclick():
-    text.configure(text=input.get(1.0, "end-1c"))
+    text.configure(text=input.get())
 
+# initializing window
 root = Tk()
-
 root.title('PyTrade')
 root.geometry('500x400')
 
-
+# button
 button = Button(root, text="Enter", fg="black", command=onclick)
 button.grid(column=1, row=1)
 
-input = Text(root, height=5, width=20)
+# text box
+input = Entry(root, width=20)
 input.grid(column=1, row=3)
 
+# text
 text = Label(text="")
 text.grid(column=1, row=4)
 
+
+# runs window
 root.mainloop()
