e897f0cb51097691d126bed733afe862104bbcb3
Abdullah-Mohammed-OTU <abdullah.mohammed@ontariotechu.net>
2023-01-21 17:12:39 -0500

    Stock class

---
+++ classForDiffStocks.py
@@ -0,0 +1,9 @@
+class StockObjects: 
+    # constructor 
+    def __init__(self, averageRateOfReturn, Industry, marketCap, dividend):
+        self.averageRateofReturn = averageRateOfReturn
+        self.Industry = Industry 
+        self.marketCap = marketCap 
+        self.dividend = dividend 
+
+    
